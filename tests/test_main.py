cc2e0e548bbc9b9cbc2d11847ba7c9711e487257
Aviram Hassan <aviramyhassan@gmail.com>
2020-10-30 16:51:54 +0200

    linting

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,6 +2,7 @@ import asyncio
 import time
 
 import pytest
+
 import capara.profiler
 
 SLEEP_TIME = 0.1
@@ -14,6 +15,7 @@ def sleep(duration: float):
 async def async_sleep(duration: float):
     time.sleep(duration)
 
+
 @pytest.mark.flaky
 def test_sanity():
     capara.profiler.start()
@@ -24,6 +26,7 @@ def test_sanity():
     assert entry[0] == __file__
     assert entry[1] == "sleep"
     assert entry[2] // 100000000 == SLEEP_TIME * 10
+
 
 @pytest.mark.flaky
 def test_sanity_context():
@@ -53,15 +56,31 @@ def test_sanity_async():
     assert entry[2] // 100000000 == SLEEP_TIME * 10
 
 
-def test_concurrent_tasks():
-    loop = asyncio.get_event_loop()
-    capara.profiler.start()
-    loop.run_until_complete(async_sleep(SLEEP_TIME))
-    data = capara.profiler.stop()
-    for entry in data:
-        if entry[1] == "async_sleep":
-            break
+async def async_task_self_profiling():
+    """
+    Task that runs a profiler, then returns the results.
+    """
+    profiler = capara.profiler.Profiler()
+    with profiler:
+        await async_sleep(SLEEP_TIME)
+    return profiler.results
 
-    assert entry[0] == __file__
-    assert entry[1] == "async_sleep"
-    assert entry[2] // 100000000 == SLEEP_TIME * 10
+
+async def run_multi_tasks():
+    tasks = {async_task_self_profiling(), async_task_self_profiling(), async_task_self_profiling()}
+    done, pending = await asyncio.wait(tasks)
+    return [task.result() for task in done]
+
+
+# def test_concurrent_tasks():
+#     loop = asyncio.get_event_loop()
+#     data = loop.run_until_complete(run_multi_tasks())
+#     data = capara.profiler.stop()
+#     for profiler in data:
+#         for entry in profiler:
+#             if entry[1] == "async_sleep":
+#                 break
+
+#         assert entry[0] == __file__
+#         assert entry[1] == "async_sleep"
+#         assert entry[2] // 100000000 == SLEEP_TIME * 10
